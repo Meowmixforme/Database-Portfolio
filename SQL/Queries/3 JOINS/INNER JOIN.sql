76ed465a47791e588c2300420d1895c6c75cd8ca
Meowmixforme <v8255920@live.tees.ac.uk>
2025-08-04 19:50:10 +0100

    Create INNER JOIN.sql

---
+++ SQL/Queries/3 JOINS/INNER JOIN.sql
@@ -0,0 +1,6 @@
+-- Retrieves payment ID, customer ID, and first name by joining payment and customer tables
+
+SELECT payment_id, payment.customer_id, first_name
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id;
