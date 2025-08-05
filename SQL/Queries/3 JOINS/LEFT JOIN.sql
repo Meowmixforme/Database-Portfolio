9679dbdcaaefa0d111e65bfea2fd1af29ab61e0c
Meowmixforme <v8255920@live.tees.ac.uk>
2025-08-05 03:24:36 +0100

    Create LEFT JOIN.sql

---
+++ SQL/Queries/3 JOINS/LEFT JOIN.sql
@@ -0,0 +1,6 @@
+-- Retrieves films that are not currently associated with any inventory records
+
+SELECT film.film_id, title, inventory_id, store_id
+FROM film
+LEFT JOIN inventory ON inventory.film_id = film.film_id
+WHERE inventory.film_id IS NULL;
