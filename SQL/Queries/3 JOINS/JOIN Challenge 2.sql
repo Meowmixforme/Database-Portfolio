3c56d204b79816f69cc80c8aa1fc5c54a0fcb98b
Meowmixforme <v8255920@live.tees.ac.uk>
2025-08-05 03:31:55 +0100

    Create JOIN Challenge 2.sql

---
+++ SQL/Queries/3 JOINS/JOIN Challenge 2.sql
@@ -0,0 +1,8 @@
+-- Retrieves titles of films featuring actor Nick Wahlberg
+
+SELECT title, first_name, last_name
+FROM film_actor
+INNER JOIN actor ON film_actor.actor_id = actor.actor_id
+INNER JOIN film ON film_actor.film_id = film.film_id
+WHERE first_name = 'Nick'
+  AND last_name = 'Wahlberg';
