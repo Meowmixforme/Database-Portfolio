cd64a51fc91af8dad02b1ce2693dc626fbd8daf7
Meowmixforme <v8255920@live.tees.ac.uk>
2025-08-09 04:13:46 +0100

    Create INSERT student.sql

---
+++ SQL/Queries/6 Creating Databases and Tables/INSERT student.sql
@@ -0,0 +1,5 @@
+-- Insert student record for 'Mark Watney'
+-- Omits optional 'email'; all required fields and constraints satisfied
+
+INSERT INTO students (first_name, last_name, homeroom_number, phone, grad_year)
+VALUES ('Mark', 'Watney', 5, '7755551234', 2035);
