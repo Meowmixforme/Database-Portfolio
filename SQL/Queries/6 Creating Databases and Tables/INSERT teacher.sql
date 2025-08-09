5bed5b7db17267e08be7e244be29aaca027304a7
Meowmixforme <v8255920@live.tees.ac.uk>
2025-08-09 04:14:32 +0100

    Create INSERT teacher.sql

---
+++ SQL/Queries/6 Creating Databases and Tables/INSERT teacher.sql
@@ -0,0 +1,6 @@
+-- Insert teacher record for 'Jonas Salk'
+-- Assigned to homeroom 5 and Biology department
+-- Email and phone are unique and valid
+
+INSERT INTO teachers (first_name, last_name, homeroom_number, department, email, phone)
+VALUES ('Jonas', 'Salk', 5, 'Biology', 'jsalk@school.org', '7755554321');
