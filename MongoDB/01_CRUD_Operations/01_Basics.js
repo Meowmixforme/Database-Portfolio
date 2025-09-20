a52dc7ac68c024bf6c8f9f371d2161f749a80fa8
Meowmixforme <v8255920@live.tees.ac.uk>
2025-09-20 02:43:05 +0100

    Create 01_Basics.js

---
+++ MongoDB/01_CRUD_Operations/01_Basics.js
@@ -0,0 +1,30 @@
+//BASICS (Windows)
+
+
+// To open MongoShell In CMD
+
+mongosh
+
+// To see existing databases
+
+show dbs
+
+// To switch to Database (Even if it doesn't exist yet)
+
+use shop
+
+// To add data to the database
+
+db.products.insertOne({name: "A Book", price: 12.99})
+
+// Find data in database
+
+db.products.find().pretty()
+
+// Add a second product with description
+
+db.products.insertOne({name: "A T-Shirt", price: 29.99, description: "This is a high quality T-Shirt"})
+
+// Add a third document with an embedded document
+
+db.products.insertOne({name: "A Computer", price: 1229.99, description: "A high quality computer", details :{cpu: "Intel i7 8770", memory: 32}}) 
