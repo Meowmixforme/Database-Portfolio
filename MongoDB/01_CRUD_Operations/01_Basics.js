5ea236da84d06ddccee40bf0816ac0a5f7bc9fc3
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-06 22:18:49 +0100

    Added flights json

--- MongoDB/01_CRUD_Operations/01_Basics.js
+++ MongoDB/01_CRUD_Operations/01_Basics.js
@@ -5,6 +5,10 @@
 
 mongosh
 
+// To specify a specific port
+
+mongosh --port 27018
+
 // To see existing databases
 
 show dbs
@@ -28,3 +32,5 @@ db.products.insertOne({name: "A T-Shirt", price: 29.99, description: "This is a 
 // Add a third document with an embedded document
 
 db.products.insertOne({name: "A Computer", price: 1229.99, description: "A high quality computer", details :{cpu: "Intel i7 8770", memory: 32}}) 
+
+// 
