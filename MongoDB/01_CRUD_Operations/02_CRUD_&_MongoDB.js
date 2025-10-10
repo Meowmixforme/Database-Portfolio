59a8e93a579fc60588bafac0aa60380344d3f082
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-10 12:04:19 +0100

    Update 02_CRUD_&_MongoDB.js

--- MongoDB/01_CRUD_Operations/02_CRUD_&_MongoDB.js
+++ MongoDB/01_CRUD_Operations/02_CRUD_&_MongoDB.js
@@ -1,1 +1,31 @@
-//
+// Insert many into a new database (see flights.json file)
+
+db.flightData.insertMany([
+   {
+     "departureAirport": "MUC",
+     "arrivalAirport": "SFO",
+     "aircraft": "Airbus A380",
+     "distance": 12000,
+     "intercontinental": true
+   },
+   {
+     "departureAirport": "LHR",
+     "arrivalAirport": "TXL",
+     "aircraft": "Airbus A320",
+     "distance": 950,
+     "intercontinental": false
+   }
+ ]
+ )
+
+// Finding (filtering) specific records
+
+db.flightData.find({intercontinental: true}).pretty()
+
+db.flightData.find({distance: {$gt: 10000}}).pretty() - gt = greater than
+
+db.flightData.find({distance: {$gt: 9000}}).pretty()
+
+// find one
+
+db.flightData.findOne({distance: {$gt: 9000}}) - first flight that matches criteria ( don't use .pretty() )
