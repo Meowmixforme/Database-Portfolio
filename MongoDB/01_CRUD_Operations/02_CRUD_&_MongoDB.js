cff981644312b656bb121e9ae5238b156004d3b6
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-10 20:45:53 +0100

    added passengers

--- MongoDB/01_CRUD_Operations/02_CRUD_&_MongoDB.js
+++ MongoDB/01_CRUD_Operations/02_CRUD_&_MongoDB.js
@@ -26,8 +26,32 @@ db.flightData.find({distance: {$gt: 10000}}).pretty() - gt = greater than
 
 db.flightData.find({distance: {$gt: 9000}}).pretty()
 
-// find one
+// Find one
 
 db.flightData.findOne({distance: {$gt: 9000}}) - first flight that matches criteria ( don't use .pretty() )
 
-  
+// Update and Update All
+
+// Update once record by id and add field delayed = true
+
+db.flightData.updateOne({_id: ObjectId('68e8e63ace54075a74cebea4')}, {$set: {delayed: true}})
+
+// Update matching id to delayed = false
+
+db.flightData.update({_id: ObjectId('68e8e63ace54075a74cebea4')}, {$set: {delayed: false}}) 
+
+// Update many using just update
+
+db.flightData.updateMany({_id: ObjectId('68e8e63ace54075a74cebea4')}, {{delayed: false}}) - this will strip all data but that specified
+
+// Update using replaceOne - is explicit way of replacing data
+
+db.flightData.replaceOne({_id: ObjectId('68e8e63ace54075a74cebea4')},{
+     "departureAirport": "MUC",
+     "arrivalAirport": "SFO",
+     "aircraft": "Airbus A380",
+     "distance": 12000,
+     "intercontinental": true
+   }) 
+
+// Find
