4723e747f8faa87128a4dd11909cfab91b35eaa4
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-12 20:59:35 +0100

    Update Document_Schemas_and_Data_Types.js

--- MongoDB/02_Schema_and_relations/Document_Schemas_and_Data_Types.js
+++ MongoDB/02_Schema_and_relations/Document_Schemas_and_Data_Types.js
@@ -2,11 +2,37 @@
 
 use shop
 
-// add a product
+// add a product (chaotic schema)
 
 db.products.insertOne({name: "A book", price: 12.99})
 
-// add another product
+// add another product with different values
 
 db.products.insertOne({title: "T-Shirt",seller: {name: "James", age: 43}})
 
+// Delete database contents
+
+db.products.deleteMany({})
+
+// Schema fixed type type
+
+db.products.insertOne({name: "A book", price: 12.99})
+
+db.products.insertOne({name: "T-Shirt", price: 20.99})
+
+
+// Schema mixed type 
+
+db.products.insertOne({name: "A Computer", price: 1299, details: {cpu: "Intel i7 8770"}}) - has same as fixed (except int price) and an extra field
+
+// Delete database contents
+
+db.products.deleteMany({})
+
+// SQL type (fixed) Assign null value for details
+
+db.products.insertOne({name: "A book", price: 12.99, details: null})
+
+db.products.insertOne({name: "T-Shirt", price: 20.99, details: null})
+
+db.products.insertOne({name: "A Computer", price: 1299, details: {cpu: "Intel i7 8770"}})
