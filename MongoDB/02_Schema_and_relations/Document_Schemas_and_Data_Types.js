9e33e15d92fb7a6e9d46f61c952c4578d6510ab6
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-14 02:09:16 +0100

    Update Document_Schemas_and_Data_Types.js

--- MongoDB/02_Schema_and_relations/Document_Schemas_and_Data_Types.js
+++ MongoDB/02_Schema_and_relations/Document_Schemas_and_Data_Types.js
@@ -36,3 +36,35 @@ db.products.insertOne({name: "A book", price: 12.99, details: null})
 db.products.insertOne({name: "T-Shirt", price: 20.99, details: null})
 
 db.products.insertOne({name: "A Computer", price: 1299, details: {cpu: "Intel i7 8770"}})
+
+
+
+// Data Types
+
+// drop the database test
+
+use test
+
+db.dropDatabase()
+
+// create a new database and populate with a company
+
+use companyData
+
+db.companies.insertOne({name: "Fresh Apples Inc", isStartup: true,employees: 33, funding: NumberDecimal("12345678901234567890"), details: {ceo: "James Super",tags: [{ title: "super" }, { title: "perfect" }], foundingDate: new Date(), insertedAt: Timestamp(Math.floor(Date.now() / 1000), 1)}})
+
+
+db.companies.findOne()
+
+// create numbers database and use  db stats to see database information
+
+db.numbers.insertOne({a: NumberInt(1)})
+
+db.stats()
+
+db.companies.drop()
+
+// to see the type of data in numbers
+
+typeof db.numbers.findOne().a
+
