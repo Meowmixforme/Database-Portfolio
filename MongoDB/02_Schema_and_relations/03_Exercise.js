a331ab9c32b340ccdcc26a1ba686476c19b98e60
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-18 02:33:49 +0100

    Add MongoDB blog exercise with users and posts
    Introduces a sample blog database with user and post collections. Inserts two users and a post with references to user ObjectIds for creator and comment author.

--- MongoDB/02_Schema_and_relations/03_Exercise.js
+++ MongoDB/02_Schema_and_relations/03_Exercise.js
@@ -1,1 +1,8 @@
-// 
+// Creating blog exercise
+
+use blog
+
+db.users.insertMany([{name: "James Smith", age: 43, email: "james@test.com"}, {name: "David Lacey", age: 78, email: "jovialmonk@test.com"}])
+
+db.posts.insertOne({title: "My first post", text: "This is my first post, I hope you like it!", tags: ["new", "tech"], creator: ObjectId('68f2ed8a1ce1cc64dfcebea5'), comments: [{text: "I like this post!", author: ObjectId('68f2ed8a1ce1cc64dfcebea4')}]}) - james smith as creator david lacey as first post
+
