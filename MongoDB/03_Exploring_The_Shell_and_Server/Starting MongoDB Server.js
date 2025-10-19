f06351a9f228d46612e21bf1f3e9db380a8455dc
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-19 05:06:44 +0100

    Add MongoDB server startup scripts and data files
    Added example scripts for starting the MongoDB server with dbpath and logpath options. Included initial WiredTiger database files, diagnostic metrics, indexes, journal, and log output to support local server startup and exploration.

--- MongoDB/03_Exploring_The_Shell_and_Server/Starting MongoDB Server.js
+++ MongoDB/03_Exploring_The_Shell_and_Server/Starting MongoDB Server.js
@@ -2,3 +2,12 @@
 
 mongod --help 
 
+// dbpath
+
+mongod --dbpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\db
+
+// logpath too
+
+mongod --dbpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\db --logpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\logs\log.log
+
+// 
