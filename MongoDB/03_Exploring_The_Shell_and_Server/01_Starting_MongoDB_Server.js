9fed6fa290e29d760c02b5b6ff6e065aeef73ba4
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-21 03:22:33 +0100

    Add mongod config and shell usage examples
    Expanded 01_Starting_MongoDB_Server.js with examples for using mongod with a config file, shell help commands, and database/collection help. Added mongod.cfg configuration file. Updated database and log files as a result of running the server with new options.

--- MongoDB/03_Exploring_The_Shell_and_Server/01_Starting_MongoDB_Server.js
+++ MongoDB/03_Exploring_The_Shell_and_Server/01_Starting_MongoDB_Server.js
@@ -10,4 +10,22 @@ mongod --dbpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\Mong
 
 mongod --dbpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\db --logpath C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\logs\log.log
 
-// 
+// Exploring Options (see mongod.cfg)
+
+// Config file (to automate)
+
+mongod -f C:\Users\Kibbl\OneDrive\Documents\GitHub\Database-Portfolio\MongoDB\03_Exploring_The_Shell_and_Server\mongod.cfg
+
+// Shell options and help
+
+mongosh --help
+
+use test
+
+// at database level
+
+db.help()
+
+// at collection level
+
+db.test.help()
