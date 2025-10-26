34d70396fb78a3365dc39852182b6540a4ba073e
Meowmixforme <v8255920@live.tees.ac.uk>
2025-10-26 20:25:52 +0000

    Add examples for insertMany with ordered option
    Added MongoDB insertMany examples for the hobbies collection, demonstrating default ordered inserts, duplicate key errors, and the use of the ordered: false option to continue inserting after errors.

--- MongoDB/04_CREATE/01_Creating_Documents.js
+++ MongoDB/04_CREATE/01_Creating_Documents.js
@@ -12,7 +12,6 @@ db.persons.insertOne({name: "James", age: 43, hobbies: ["Guitar", "Walking"]})
 
 db.persons.insertOne({name: "David", age: 74, hobbies: ["Complaining", "Pooping"]})
 
-
 db.persons.insertMany([{name: "Anna", age: 29, hobbies: ["Sports", "Yoga"]}])
 
 db.persons.insertMany([{name: "Maria", age: 31,}, {name: "Chris", age: 25}])
@@ -24,4 +23,19 @@ db.persons.find().pretty()
 db.persons.insertMany([{name: "Sandeep", age: 28,}, {name: "Hans", age: 38}])
 
 // ordered inserts
+
+db.hobbies.insertMany([{_id: "sports", name: "Sports"}, {_id: "cooking", name: "Cooking"}, {_id: "cars", name: "Cars"}])
+
+db.hobbies.find().pretty()
+
+// will fail for last 2 as _id "cooking" already exists (duplicate key). Yoga will insert but fail and stop after cooking.
+
+db.hobbies.insertMany([{_id: "yoga", name: "Yoga"}, {_id: "cooking", name: "Cooking"}, {_id: "hiking", name: "Hiking"}])
+
+db.hobbies.find().pretty()
+
+// to override behaviour disable ordered insert. It now fails for first two duplicates but will add Hiking instead of quitting 
+
+db.hobbies.insertMany([{_id: "yoga", name: "Yoga"}, {_id: "cooking", name: "Cooking"}, {_id: "hiking", name: "Hiking"}], {ordered: false})
+
 
