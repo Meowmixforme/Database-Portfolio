93d833fd7b2860c10d4243291ee696e0d1123d8e
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-03 02:07:01 +0000

    Add MongoDB comparison operator query examples
    Expanded the reading data script to include examples of MongoDB queries using comparison operators such as $eq, $ne, $lt, $lte, and $gte for filtering documents by runtime.

--- MongoDB/05_READ/01_Reading_data.js
+++ MongoDB/05_READ/01_Reading_data.js
@@ -13,4 +13,18 @@ db.movies.find().pretty()
 
 db.movies.find({runtime: 60}) - filter equality
 
-//
+// Comparison operators
+
+db.movies.find({runtime: {$eq: 60}}) - equals
+
+db.movies.find({runtime: {$ne: 60}}) - not equals
+
+db.movies.find({runtime: {$lt: 40}}).pretty() - lower than
+
+db.movies.find({runtime: {$lte: 42}}).pretty() - lower than or equal
+
+db.movies.find({runtime: {$gte: 42}}).pretty() - greater than
+
+db.movies.find({runtime: {$gte: 42}}).pretty() - greater than or equal
+
+
