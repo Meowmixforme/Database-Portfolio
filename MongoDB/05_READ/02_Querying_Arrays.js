fc7d5b2b72a73fcb27fdf6f311f7b32356230eff
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-10 00:02:10 +0000

    Add MongoDB array querying examples and sample data
    Added example queries for array fields in MongoDB, including $size and $all operators, and provided a sample boxoffice.json dataset for use with mongoimport. These changes support learning and demonstration of querying arrays and embedded documents.

--- MongoDB/05_READ/02_Querying_Arrays.js
+++ MongoDB/05_READ/02_Querying_Arrays.js
@@ -1,1 +1,32 @@
+// Assignment
+
+// import data
+
+mongoimport --file boxoffice.json --db boxOffice --collection movieStarts --jsonArray --drop
+
+use boxOffice
+
+db.movieStarts.find({"meta.rating": { $gt: 9.2}, "meta.runtime": { $lt: 100}}).pretty()
+
+db.movieStarts.find({$or: [{genre: "drama"}, {genre: "action"}]}).pretty()
+
+db.movieStarts.find({$expr: {$gt: ["$visitors", "$expectedVisitors"]}}).pretty()
+
 // Querying_Arrays
+
+use user
+
+db.users.find({"hobbies.title": "Sports"}).pretty()
+
+// $size
+
+db.users.insertOne({name: "Chris", hobbies: ["Sports", "Cooking", "Hiking"]})
+
+db.users.find({hobbies: {$size: 3}}).pretty() - all users who have three hobbies (Chris) - must be exact number
+
+// $all
+
+use boxOffice
+
+db.movieStarts.find({genre: {$all: ["action", "thriller"]}}).pretty() - genre of exactly 'thriller' and 'action', don't care about the order
+
