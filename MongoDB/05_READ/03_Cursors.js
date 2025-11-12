75623fe2e105ea36abd8111bf1068599e2ec6905
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-12 04:01:07 +0000

    Add MongoDB projection and array projection examples
    Expanded the cursor examples to include various projection techniques, such as selecting specific fields, projecting embedded fields, using array projections with $elemMatch and $slice, and shaping query results. These examples demonstrate how to control the output of find queries in MongoDB.

--- MongoDB/05_READ/03_Cursors.js
+++ MongoDB/05_READ/03_Cursors.js
@@ -30,3 +30,23 @@ db.movies.find().sort({"rating.average": 1, runtime: -1}).skip(10).limit(10).pre
 
 // Using projection to shape results
 
+db.movies.find({}, {name: 1, genres: 1, rating: 1}).pretty() - only fields listed with 1 are shown (plus the id)
+
+db.movies.find({}, {name: 1, genres: 1, "rating.average": 1, "schedule.time": 1}).pretty() - embedded
+
+// Using projection in Arrays
+
+db.movies.find({genres: "Drama"}, {"genres.$": 1}).pretty() - only shows Drama
+
+db.movies.find({genres: {$all: ["Drama", "Horror"]}}, {"genres.$": 1}).pretty() - only retrieve Horror
+
+db.movies.find({genres: "Drama"}, {genres: {$elemMatch: {$eq: "Horror"}}}).pretty() - control which items in an array are displayed
+
+db.movies.find({"rating.average": {$gt: 9}}, {genres: {$elemMatch: {$eq: "Horror"}}}).pretty() - with rating greater than 9
+
+// $slice
+
+db.movies.find({"rating.average": {$gt: 9}}, {genres: {$slice: 2}, name: 1}).pretty() - List of items with only first two elements
+
+db.movies.find({"rating.average": {$gt: 9}}, {genres: {$slice: [1, 2]}, name: 1}).pretty() - skips first item and returns items 2 and 3
+
