15dcc9c358d98158f711816ab375a3fc374e1603
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-14 01:02:11 +0000

    Add examples for updating array elements in MongoDB
    Added queries demonstrating how to find and update array elements in MongoDB documents, including updating the first matching element and all elements in an array based on conditions.

--- MongoDB/06_UPDATE/02_Update_Arrays.js
+++ MongoDB/06_UPDATE/02_Update_Arrays.js
@@ -10,3 +10,11 @@ db.users.updateMany({hobbies: {$elemMatch: {title: "Sports", frequency: {$gte: 3
 
 // Update all Array elements
 
+db.users.find({"hobbies.frequency": {$gt: 2}}).pretty()
+
+db.users.updateMany({"hobbies.frequency": {$gt: 2}}, {$set: {"hobbies.$.goodFrequency": true}}) - Only updates the first element for all if freqency > 2
+
+db.users.find({totalAge: {$gt: 30}})
+
+db.users.updateMany({totalAge: {$gt: 30}}, {$inc: {"hobbies.$[].frequency": -1}}) - update all elements in array to decrease frequency by 1 
+
