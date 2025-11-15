a8f2788c99f828f94f3432eb3d650accb2a1e2b5
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-15 00:33:06 +0000

    Add examples for compound and unique indexes in MongoDB
    Expanded the index tutorial with examples of compound indexes, sorting with indexes, viewing default indexes, configuring unique indexes, and partial filter placeholders. These additions provide more comprehensive coverage of MongoDB index features.

--- MongoDB/08_Indexes/01_Indexes.js
+++ MongoDB/08_Indexes/01_Indexes.js
@@ -19,3 +19,39 @@ db.contacts.createIndex({"dob.age": 1}) - create an index and sort ascending (ca
 db.contacts.explain("executionStats").find({"dob.age": {$gt: 20}}) 
 
 db.contacts.dropIndex({"dob.age": 1}) - remove index (can be faster for < 20 search as extra index step was removed)
+
+// Compound indexes
+
+db.contacts.createIndex({gender: 1})
+
+db.contacts.explain("executionStats").find({gender: "male"})
+
+db.contacts.dropIndex({gender: 1})
+
+db.contacts.createIndex({"dob.age": 1, gender: 1}) - Compound = index with more than one field
+
+db.contacts.explain().find({"dob.age": 35, gender: "male"}) query for males who are 35
+
+db.contacts.explain().find({"dob.age": 35}) - just query age
+
+// Using indexes for sorting
+
+db.contacts.explain().find({"dob.age": 35}).sort({gender: 1}) - search for all males of 35 in an ascending order
+
+// Default index
+
+db.contacts.getIndexes() - see all indexes (default is id_1)
+
+// Configuring indexes
+
+db.contacts.createIndex({email: 1}, {unique: true}) - finds duplicate key for "abigail.clark@example.com"
+
+db.contacts.find({email: "abigail.clark@example.com"}).count() - 2 people with the id
+
+// Partial filters
+
+
+
+
+
+
