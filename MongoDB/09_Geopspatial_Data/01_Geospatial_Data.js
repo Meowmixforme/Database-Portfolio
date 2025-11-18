06163625c98639439cb98574a30e9161e36c7002
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-18 06:39:30 +0000

    Add geospatial data and queries example
    Expanded the geospatial data script to include GeoJSON data insertion, creation of a 2dsphere index, and example queries using $near for proximity searches. This provides a practical demonstration of geospatial features in MongoDB.

--- MongoDB/09_Geopspatial_Data/01_Geospatial_Data.js
+++ MongoDB/09_Geopspatial_Data/01_Geospatial_Data.js
@@ -1,2 +1,20 @@
 // Geospatial data
 
+// Adding GeoJSON data
+
+use awesomeplaces
+
+37.7689412,-122.4759107 - California Academy of Sciences
+
+db.places.insertOne({name: "California Academy of Sciences", location: {type: "Point", coordinates: [-122.4759107, 37.7689412]}}) - long then lat
+
+db.places.findOne()
+
+// Running GeoJson queries
+
+37.770282,-122.4724223 - Fake current location
+
+db.places.createIndex({location: "2dsphere"}) - geospatial index (required for queries $near, $geoWithin and $geoIntersects)
+
+db.places.find({location: {$near: {$geometry: {type: "Point", coordinates: [-122.4724223, 37.770282]}, $maxDistance: 500, $minDistance: 10}}}).pretty() - returns places near Fake current location within a defined radius
+
