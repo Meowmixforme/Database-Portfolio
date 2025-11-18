285fc9e160e3d769ee0ced2af4d7efc5db6aac69
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-18 12:22:25 +0000

    Add sample geospatial place documents
    Inserted three new place documents with geospatial coordinates into the 'places' collection for demonstration and testing of geospatial queries.

--- MongoDB/09_Geopspatial_Data/01_Geospatial_Data.js
+++ MongoDB/09_Geopspatial_Data/01_Geospatial_Data.js
@@ -18,3 +18,10 @@ db.places.createIndex({location: "2dsphere"}) - geospatial index (required for q
 
 db.places.find({location: {$near: {$geometry: {type: "Point", coordinates: [-122.4724223, 37.770282]}, $maxDistance: 500, $minDistance: 10}}}).pretty() - returns places near Fake current location within a defined radius
 
+db.places.insertOne({name: "Conservatory of Flowers", location: {type: "Point", coordinates: [-122.4641855, 37.7702029]}})
+
+db.places.insertOne({name: "Lisa & Douglas Goldman Tennis Center", location: {type: "Point", coordinates: [-122.461841, 37.7695612]}})
+
+db.places.insertOne({name: "The Painted Ladies", location: {type: "Point", coordinates: [-122.4483471, 37.7622779]}}) - Outside Golden Gate Park
+
+
