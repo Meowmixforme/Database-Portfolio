341d2dcbbb372935789110af00decf0aa1f66933
Meowmixforme <v8255920@live.tees.ac.uk>
2025-11-19 15:56:02 +0000

    Add aggregation pipeline examples for MongoDB
    Added examples demonstrating the use of $match, $group, and $sort stages in the aggregation pipeline to filter, group, and sort persons by gender and state. These examples illustrate how to count and order female persons by their state.

--- MongoDB/10_Aggregation_Framework/01_Aggregation_Pipeline.js
+++ MongoDB/10_Aggregation_Framework/01_Aggregation_Pipeline.js
@@ -10,3 +10,25 @@ use analytics
 show collections
 
 db.persons.findOne()
+
+// Match
+
+db.persons.aggregate([{$match: {gender: "female"}}]).pretty() - only show females 
+
+// Group
+
+db.persons.aggregate([
+    { $match: { gender: 'female' } },
+    { $group: { _id: { state: "$location.state" }, totalPersons: { $sum: 1 } } }
+]).pretty(); - total number of females by their state 
+
+db.persons.aggregate([
+    { $match: { gender: 'female' } },
+    { $group: { _id: { state: "$location.state" }, totalPersons: { $sum: 1 } } },
+    { $sort: { totalPersons: -1 } }
+]).pretty(); - sort by total people (descending)
+
+// Assignment
+
+
+
