ef548b588aa363dcdb9dff3bb00a8b232795d3c4
Meowmixforme <meowmixforme@gmail.com>
2025-11-20 09:19:29 +0000

    Move advanced aggregation pipeline to main file
    Transferred the aggregation pipeline that converts location to GeoJSON and splits birthdate fields from commands-4.js into 01_Aggregation_Pipeline.js. Removed the now redundant commands-4.js file for better organization and consolidation of aggregation examples.

--- MongoDB/10_Aggregation_Framework/01_Aggregation_Pipeline.js
+++ MongoDB/10_Aggregation_Framework/01_Aggregation_Pipeline.js
@@ -74,3 +74,68 @@ db.persons.aggregate([
     }
   ]).pretty(); - create new fiels fullname using first and lastname
 
+// Changing a location into GeoJSON object and birthdate into different fields
+
+db.persons.aggregate([
+    {
+      $project: {
+        _id: 0,
+        name: 1,
+        email: 1,
+        birthdate: { $convert: { input: '$dob.date', to: 'date' } },
+        age: "$dob.age",
+        location: {
+          type: 'Point',
+          coordinates: [
+            {
+              $convert: {
+                input: '$location.coordinates.longitude',
+                to: 'double',
+                onError: 0.0,
+                onNull: 0.0
+              }
+            },
+            {
+              $convert: {
+                input: '$location.coordinates.latitude',
+                to: 'double',
+                onError: 0.0,
+                onNull: 0.0
+              }
+            }
+          ]
+        }
+      }
+    },
+    {
+      $project: {
+        gender: 1,
+        email: 1,
+        location: 1,
+        birthdate: 1,
+        age: 1,
+        fullName: {
+          $concat: [
+            { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+            {
+              $substrCP: [
+                '$name.first',
+                1,
+                { $subtract: [{ $strLenCP: '$name.first' }, 1] }
+              ]
+            },
+            ' ',
+            { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+            {
+              $substrCP: [
+                '$name.last',
+                1,
+                { $subtract: [{ $strLenCP: '$name.last' }, 1] }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  ]).pretty();
+
